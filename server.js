599b6d7c0b61a2f79ad63d1de9fdd50018f94436
kajdav <poppa.bear.joe@gmail.com>
2014-10-21 16:25:42 -0600

    "finishes project"

--- server.js
+++ server.js
@@ -1,7 +1,79 @@
-var express = require('express');
+var Express = require('express'),
+	Mongoose = require('mongoose'),
+	bodyParser = require('body-parser'),
+	connection = Mongoose.connection,
+	mongoUri = 'mongodb://localhost/song',
+	port = 9011,
+	app = Express();
 
-var app = express();
+var Artist = require('./lib/artist'),
+	Song = require('./lib/song'),
+	Tag = require('./lib/tag');
 
-//routes go here
+Mongoose.connect(mongoUri);
+connection.once('open', function(){
+	console.log('Successfully connected to ' + mongoUri);
+});
 
-app.listen(8000);
+
+app.use(bodyParser.json());
+
+app.post('/artists', function(req, res){
+	var artist = new Artist(req.body.artist);
+	artist.save(function(err){
+		if(err) {res.send(err)};
+		res.send(req.body.artist.name + ' was saved!');
+	});
+});
+
+app.post('/artists/:name/songs', function(req, res){
+	Artist.findOne({ name: req.params.name }).populate('songs').exec(function(err, artist){
+		if(err){res.send(err)};
+		var newSong = new Song(req.body.song);
+		newSong.save(function(err, song){
+			if (err) {res.send(err)};
+			artist.songs.push(newSong);
+			artist.save(function(err, artist){
+				if(err){res.send(err)};
+				res.send(song.name + ' was saved to ' + artist.name + "'s collection!");
+			})
+		})
+	});
+});
+
+app.post('/songs/:name/tags', function(req, res){
+	Tag.findOneAndUpdate({ name: req.body.name}, req.body, {upsert: true})
+		.exec(function(err, tag){
+			if(err) res.send(err);
+			tag.save(function(err){
+				if(err) res.send(err);
+			});
+			Song.findOne({name: req.params.name})
+				.exec(function(err, song){
+					if(err) return res.send(err);
+					song.tags.addToSet(tag);
+					song.save(function(err){
+						if(err) res.send(err);
+						res.send(tag.name + ' has been added to ' + song.name);
+					})
+		})
+	});
+});
+
+app.get('/artists', function(req, res){
+	Artist.find(function(err, artist){
+		if(err){res.send(err)};
+		res.send(artist);
+	});
+});
+
+app.get('/artists/:name', function(req, res){
+	Artist.findOne({ name: req.params.name }, function(err, artist){
+		if(err){res.send(err)};
+		res.send(artist);
+	});
+});
+
+app.listen(port, function(){
+	console.log('Now listening on port: ' + port)
+});
